3c2efb2c9ef47ae35e80841408e1fd4098dead63
GKentaurus <gkent@outlook.com>
2020-08-01 14:06:19 -0500

    Iniciando el proyecto en NetBeans

---
+++ src/Cliente.java
@@ -0,0 +1,10 @@
+/**
+ * BootCamp SENA Agosto 2020
+ * 
+ * @author Danniela Riaño Huertas
+ * @author Veronica Ortiz Rocha
+ * @author Carlos Moreno Cifuentes
+ */
+public class Cliente {
+    
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * BootCamp SENA Agosto 2020
+ * 
+ * @author Danniela Riaño Huertas
+ * @author Veronica Ortiz Rocha
+ * @author Carlos Moreno Cifuentes
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
